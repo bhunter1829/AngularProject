53522be790cd6b8a3ccb899ded04db19b0aee1c9
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-20 20:41:55 -0500

    no changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetAllComponent } from './component/get-all/get-all.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetAllComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
