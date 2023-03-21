46fd28b1125798df9f5a56ba3ff3fb6d6bb12957
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-20 20:46:59 -0500

    added html logic and routing

---
+++ src/app/component/get-all/get-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all',
+  templateUrl: './get-all.component.html',
+  styleUrls: ['./get-all.component.css']
+})
+export class GetAllComponent {
+  
+}
