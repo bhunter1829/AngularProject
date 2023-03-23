bbf32568ac96bdf52e4ade3443c9e1ecc0822bd9
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-23 14:39:01 -0500

    added method getAddInventory

--- src/app/service/inventory.service.ts
+++ src/app/service/inventory.service.ts
@@ -17,7 +17,12 @@ getAllInventory():Observable<any[]>{
   return this.http.get<any[]>("http://127.0.0.1:9000/inventory",{headers:header});
 }
 
-
+getAddInventory(inventory: Inventory):Observable<Inventory>{
+  let header: HttpHeaders = new HttpHeaders();
+  header.append("accept", "text/json");
+  header.append("Access-Control-Allow-Origin", "*");
+  return this.http.post<Inventory>("http://127.0.0.1:9000/inventory", inventory,{headers:header}); 
+}
 
   constructor(private http: HttpClient) { }
 }
