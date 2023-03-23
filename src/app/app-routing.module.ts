47d13dcbb2933a28f63131f4dd316c35759fe1d1
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-23 14:40:59 -0500

    added new route to add-inventory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddInventoryComponent } from './component/add-inventory/add-inventory.component';
 import { GetAllComponent } from './component/get-all/get-all.component';
 
 const routes: Routes = [
- {path:"get-all", component: GetAllComponent}
+ {path:"get-all", component: GetAllComponent},
+ {path:"add-inventory", component:AddInventoryComponent }
 ];
 
 @NgModule({
