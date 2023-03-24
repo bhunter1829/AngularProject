10f1ee8eff47d652c0301c7905baa850e9662f87
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-24 14:24:31 -0500

    renamed deletebyID to deleteByName

--- src/app/service/inventory.service.ts
+++ src/app/service/inventory.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable, throwError } from 'rxjs';
 import { Inventory } from '../models/inventory';
@@ -18,11 +18,11 @@ getAllInventory():Observable<any[]>{
 }
 
 
-deleteInventoryById(id : number ):Observable<any[]>{
+deleteInventoryByName(name : string ):Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.delete<any[]>("http://127.0.0.1:9000/inventory/" + id ,{headers:header});
+  return this.http.delete<any[]>("http://127.0.0.1:9000/inventory/${name}" + name ,{headers:header})
 }
 
 
