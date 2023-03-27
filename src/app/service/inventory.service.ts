1720ee9da034332baf32588b16da7f38b3c72119
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-27 15:38:12 -0500

    added the stupid back ticks that work now for some readson

--- src/app/service/inventory.service.ts
+++ src/app/service/inventory.service.ts
@@ -22,7 +22,7 @@ deleteInventoryByName(name : string ):Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.delete<any[]>("http://127.0.0.1:9000/inventory/${name}" + name ,{headers:header})
+  return this.http.delete<any[]>(`http://127.0.0.1:9000/inventory/${name}` ,{headers:header})
 }
 
 
