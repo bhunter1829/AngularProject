5ee464a35aa9cff7a0c1084a9ae17804ec9c247e
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-31 16:15:24 -0500

    fixed the login button to now show the user

--- src/app/component/login-form/login-form.component.ts
+++ src/app/component/login-form/login-form.component.ts
@@ -12,18 +12,10 @@ export class LoginFormComponent implements OnInit{
   password : string = "";
   currentUser : User = this.authService.user;
 
-  submit():void{
-   // this.authService.login(this.username, this.password);
-    this.currentUser = this.authService.user;
-    this.authService.login(this.username,this.password);
-    let user : User={username:this.username, password:this.password};
-    this.authService.login(user).subscribe(json=> {this.username = json;})
-    // let promise = new Promise((resolve, reject) => {
-    //   reject('Promise rejected!')
-    // });
-    
-    // promise.catch(error => console.log(error));
-    // promise.catch(error => console.log(error));
+  submit(){
+
+    this.authService.login(this.username, this.password).subscribe(json =>{this.authService.user=json; this.currentUser=json})
+
   }
 
   constructor(private authService : AuthService){}
@@ -32,3 +24,15 @@ export class LoginFormComponent implements OnInit{
 
   }
 }
+
+
+//user stories 
+
+//register same page on login 
+//connects to the register backend 
+//make it interact with the backend 400 
+
+
+//brenden does this stuffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+//adjustingn inven amount to decrease or increase. 
+// inventory list search input box that searches for a sepecifc name
