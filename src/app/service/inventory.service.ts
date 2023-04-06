5a2f63b62afc0ef60578207ea233646a3129ae33
brenden hunter <hunterb7@gmatc.matc.edu>
2023-04-06 10:25:19 -0500

    Update

--- src/app/service/inventory.service.ts
+++ src/app/service/inventory.service.ts
@@ -15,7 +15,8 @@ getAllInventory():Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.get<any[]>("http://127.0.0.1:9000/inventory",{headers:header});
+  // return this.http.get<any[]>("http://127.0.0.1:9000/inventory",{headers:header});
+  return this.http.get<any[]>("http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/inventory",{headers:header});
 }
 
 
@@ -23,14 +24,16 @@ deleteInventoryByName(name : string ):Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.delete<any[]>(`http://127.0.0.1:9000/inventory/remove/${name}` ,{headers:header})
+  // return this.http.delete<any[]>(`http://127.0.0.1:9000/inventory/remove/${name}` ,{headers:header})
+  return this.http.delete<any[]>(`http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/inventory/remove/${name}` ,{headers:header})
 }
 
 getInventoryByName(name : string ):Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.get<any[]>(`http://127.0.0.1:9000/inventory/${name}` ,{headers:header})
+  // return this.http.get<any[]>(`http://127.0.0.1:9000/inventory/${name}` ,{headers:header})
+  return this.http.get<any[]>(`http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/inventory/${name}` ,{headers:header})
 }
 
 
@@ -40,7 +43,8 @@ getAddInventory(inventory: Inventory):Observable<Inventory>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.post<Inventory>("http://127.0.0.1:9000/inventory", inventory,{headers:header}); 
+  // return this.http.post<Inventory>("http://127.0.0.1:9000/inventory", inventory,{headers:header});
+  return this.http.post<Inventory>("http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/inventory", inventory,{headers:header}); 
 }
 
 getUniqueCount():Observable<number>{
@@ -48,7 +52,8 @@ getUniqueCount():Observable<number>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.get<number>("http://127.0.0.1:9000/count",{headers:header});
+  // return this.http.get<number>("http://127.0.0.1:9000/count",{headers:header});
+  return this.http.get<number>("http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/count",{headers:header});
 }
 
 getTotalItems():Observable<number>{
@@ -56,15 +61,19 @@ getTotalItems():Observable<number>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.get<number>("http://127.0.0.1:9000/sum",{headers:header});
+  // return this.http.get<number>("http://127.0.0.1:9000/sum",{headers:header});
+  return this.http.get<number>("http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/sum",{headers:header});
 }
 
 patchItemAmount(name : string, amount : number):Observable<any[]>{
   let header: HttpHeaders = new HttpHeaders();
   header.append("accept", "text/json");
   header.append("Access-Control-Allow-Origin", "*");
-  return this.http.patch<any[]>(`http://127.0.0.1:9000/inventory/${name}/${amount}` ,{headers:header})
+  // return this.http.patch<any[]>(`http://127.0.0.1:9000/inventory/${name}/${amount}` ,{headers:header})
+  return this.http.patch<any[]>(`http://ec2-54-204-180-31.compute-1.amazonaws.com:9000/inventory/${name}/${amount}` ,{headers:header})
 }
+
+// http://ec2-54-204-180-31.compute-1.amazonaws.com
 
 
 
